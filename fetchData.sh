d65c2baca9d67b81ae17e86014e95a2637988555
WorkerBee <martinschroeder13@yahoo.de>
2021-04-14 12:58:42 +0200

    add Data

--- fetchData.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -d /tmp/fiveringsdb-data ]; then
-    mkdir /tmp/fiveringsdb-data
-    git clone "https://github.com/fatihi/fiveringsdb-data" /tmp/fiveringsdb-data
-else
-    cd /tmp/fiveringsdb-data
-    git pull master
-fi
