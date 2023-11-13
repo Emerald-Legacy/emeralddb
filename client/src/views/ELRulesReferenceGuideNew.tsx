8ba6bfdc109c0ac8eb821d892633b2e022007ca8
WorkerBee <martinschroeder13@yahoo.de>
2023-11-13 19:27:24 +0100

    use ascii-doc rules document

---
+++ client/src/views/ELRulesReferenceGuideNew.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from "react";
+import asciidoctor from "asciidoctor";
+import { Box, Button, Dialog, DialogActions, DialogContent, Fab, Grid, useMediaQuery } from "@material-ui/core";
+import TocIcon from "@material-ui/icons/Toc";
+
+export function ELRulesReferenceGuideNew(): JSX.Element {
+  const [content, setContent] = useState('Loading')
+  const [toc, setToc] = useState('Loading')
+  const [showTable, setShowTable] = useState(false)
+  const isSmOrBigger = useMediaQuery('(min-width:600px)')
+  const asciiDocUrl = 'https://raw.githubusercontent.com/Emerald-Legacy/rules-documents/main/docs/Rules%20Reference%20Guide.adoc'
+
+  useEffect(() => {
+    const myRequest = new Request(asciiDocUrl);
+    fetch(myRequest)
+      .then(resp => resp.text())
+      .then(text => reformatContent(text))
+  }, [])
+
+  const reformatContent = (asciiDoc: string) => {
+    // @ts-ignore
+    const asciiDoctor = asciidoctor();
+    const convertedHtml = asciiDoctor.convert(asciiDoc)
+      .toString()
+      .replaceAll("[military]", '<span class=\"icon icon-conflict-military\"></span>')
+      .replaceAll("[political]", '<span class=\"icon icon-conflict-political\"></span>')
+      .replaceAll("[Air]", '<span class=\"icon icon-element-air\"></span>')
+      .replaceAll("[Earth]", '<span class=\"icon icon-element-earth\"></span>')
+      .replaceAll("[Fire]", '<span class=\"icon icon-element-fire\"></span>')
+      .replaceAll("[Void]", '<span class=\"icon icon-element-void\"></span>')
+      .replaceAll("[Water]", '<span class=\"icon icon-element-water\"></span>')
+      .replaceAll("[Water]", '<span class=\"icon icon-element-water\"></span>')
+      .replaceAll("class=\"emeralddb\">&#59655;", 'class=\"icon icon-clan-crab\"')
+      .replaceAll("class=\"emeralddb\">&#59656;", 'class=\"icon icon-clan-crane\"')
+      .replaceAll("class=\"emeralddb\">&#59657;", 'class=\"icon icon-clan-dragon\"')
+      .replaceAll("class=\"emeralddb\">&#59658;", 'class=\"icon icon-clan-lion\"')
+      .replaceAll("class=\"emeralddb\">&#59659;", 'class=\"icon icon-clan-phoenix\"')
+      .replaceAll("class=\"emeralddb\">&#59660;", 'class=\"icon icon-clan-scorpion\"')
+      .replaceAll("class=\"emeralddb\">&#59661;", 'class=\"icon icon-clan-unicorn\"');
+
+    console.log(convertedHtml)
+
+    const splitted = convertedHtml.split('<div id="preamble">')
+    const tableOfContents = splitted[0]
+    const document = splitted[1]
+    setToc(tableOfContents)
+    setContent(document)
+  }
+
+  const TableOfContents = () => (
+    <div dangerouslySetInnerHTML={{__html: toc}}/>
+  )
+
+  return (
+    <Grid container spacing={3} direction={isSmOrBigger ? 'row' : 'column-reverse'}>
+      <Grid item sm={8}>
+        <Box style={{ maxHeight: isSmOrBigger ? '93vh' : '85vh', overflow: 'auto' }} p={1}>
+          <div dangerouslySetInnerHTML={{__html: content}} />
+        </Box>
+      </Grid>
+      <Grid item sm={4}>
+        <Box p={1}>
+          {!isSmOrBigger ? (
+            <>
+              <Fab
+                variant="extended"
+                color={'secondary'}
+                onClick={() => setShowTable(true)}
+                style={{position: 'absolute'}}
+              >
+                <TocIcon style={{ marginRight: '10px' }} />
+                Table of Contents
+              </Fab>
+              <Dialog open={showTable} keepMounted onClose={() => setShowTable(false)}>
+                <DialogContent>
+                  <TableOfContents />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={() => setShowTable(false)} color="secondary">
+                    Close
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </>
+          ) : (
+            <Box style={{ maxHeight: '93vh', overflow: 'auto' }}>
+              <TableOfContents />
+            </Box>
+          )}
+        </Box>
+      </Grid>
+    </Grid>
+  )
+}
