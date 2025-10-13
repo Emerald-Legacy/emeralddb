b7f1b318ac2e34cdfcc5962762c1cd183276b540
Benjamin Wolf <benjamin.wolf@innoq.com>
2025-10-13 22:54:29 +0200

    Fix login
    auth0 v2 requires a different method call

--- client/src/components/usermenu/LoginButton.tsx
+++ client/src/components/usermenu/LoginButton.tsx
@@ -1,14 +1,25 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { Button } from '@material-ui/core'
 
-export function LoginButton(props: { onLogin: () => void }): JSX.Element {
+export function LoginButton(props: {
+  onLogin: () => void
+  audience: string
+  scope: string
+}): JSX.Element {
   const { loginWithPopup } = useAuth0()
 
   return (
     <Button
       variant="contained"
       color="secondary"
-      onClick={() => loginWithPopup().then(props.onLogin)}
+      onClick={() =>
+        loginWithPopup({
+          authorizationParams: {
+            audience: props.audience,
+            scope: props.scope,
+          },
+        }).then(props.onLogin)
+      }
     >
       Log In
     </Button>
