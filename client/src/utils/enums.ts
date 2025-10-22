12896c4b9b21f203e9d31bd2e1ec480ad3978fb2
David Damilola <37887398+dagbolade@users.noreply.github.com>
2025-10-22 10:29:40 +0100

    Fix enums ordering for roleRestrictions and factions as per review feedback

--- client/src/utils/enums.ts
+++ client/src/utils/enums.ts
@@ -1,31 +1,32 @@
 export const cardTypes: { id: string; name: string }[] = [
-  { id: 'attachment', name: 'Attachment' },
   { id: 'character', name: 'Character' },
+  { id: 'attachment', name: 'Attachment' },
   { id: 'event', name: 'Event' },
   { id: 'holding', name: 'Holding' },
-  { id: 'province', name: 'Province' },
-  { id: 'role', name: 'Role' },
   { id: 'stronghold', name: 'Stronghold' },
+  { id: 'province', name: 'Province' },
   { id: 'treaty', name: 'Treaty' },
+  { id: 'role', name: 'Role' },
   { id: 'warlord', name: 'Warlord' },
 ]
 
 export const sides: { id: string; name: string }[] = [
-  { id: 'conflict', name: 'Conflict' },
   { id: 'dynasty', name: 'Dynasty' },
+  { id: 'conflict', name: 'Conflict' },
   { id: 'province', name: 'Province' },
   { id: 'role', name: 'Role' },
   { id: 'treaty', name: 'Treaty' },
 ]
 
+// Elements first, then Keeper and Seeker
 export const roleRestrictions: { id: string; name: string }[] = [
   { id: 'air', name: 'Air' },
   { id: 'earth', name: 'Earth' },
   { id: 'fire', name: 'Fire' },
-  { id: 'keeper', name: 'Keeper' },
-  { id: 'seeker', name: 'Seeker' },
   { id: 'void', name: 'Void' },
   { id: 'water', name: 'Water' },
+  { id: 'keeper', name: 'Keeper' },
+  { id: 'seeker', name: 'Seeker' },
 ]
 
 export const elements: { id: string; name: string }[] = [
@@ -36,17 +37,18 @@ export const elements: { id: string; name: string }[] = [
   { id: 'water', name: 'Water' },
 ]
 
+// Seven clans, then neutral, then shadowlands
 export const factions: { id: string; name: string; color: string }[] = [
   { id: 'crab', name: 'Crab', color: '#163078' },
   { id: 'crane', name: 'Crane', color: '#44c2bc' },
   { id: 'dragon', name: 'Dragon', color: '#1d6922' },
   { id: 'lion', name: 'Lion', color: '#dece23' },
   //{ id: 'mantis', name: 'Mantis', color: '#2c8369' },
-  { id: 'neutral', name: 'Neutral', color: '#b1b1b1' },
   { id: 'phoenix', name: 'Phoenix', color: '#de9923' },
   { id: 'scorpion', name: 'Scorpion', color: '#ab1916' },
-  { id: 'shadowlands', name: 'Shadowlands', color: '#000000' },
   { id: 'unicorn', name: 'Unicorn', color: '#90119e' },
+  { id: 'neutral', name: 'Neutral', color: '#b1b1b1' },
+  { id: 'shadowlands', name: 'Shadowlands', color: '#000000' },
 ]
 
 export const clans: { id: string; name: string }[] = [
