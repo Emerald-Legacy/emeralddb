2732cef24490dd16334a6cd7d1921cf2c4f705f0
Benjamin Wolf <benjamin.wolf.de@me.com>
2025-11-17 17:30:27 +0100

    Fix caching when unpublishing a list

--- client/src/components/deck/DecklistTabs.tsx
+++ client/src/components/deck/DecklistTabs.tsx
@@ -100,7 +100,7 @@ export function DecklistTabs(props: {
   const { enqueueSnackbar } = useSnackbar()
   const queryClient = useQueryClient()
 
-  const publishMutation = useMutation({ 
+  const publishMutation = useMutation({
     mutationFn: (decklistId: string) => privateApi.Decklist.publish({ decklistId }),
     onSuccess: () => {
       props.onDecklistUpdated()
@@ -135,6 +135,7 @@ export function DecklistTabs(props: {
             props.onDecklistUpdated()
             setCurrentDecklistId(latestDecklistForDeck(deck)?.id || undefined)
             enqueueSnackbar('The decklist was deleted successfully!', { variant: 'success' })
+            queryClient.invalidateQueries({ queryKey: UiStoreQueries.PUBLISHED_DECKLISTS })
           })
           .catch((error) => {
             const message = error.data()
@@ -169,6 +170,7 @@ export function DecklistTabs(props: {
           .then(() => {
             props.onDecklistUpdated()
             enqueueSnackbar('The decklist was unpublished successfully!', { variant: 'success' })
+            queryClient.invalidateQueries({ queryKey: UiStoreQueries.PUBLISHED_DECKLISTS })
           })
           .catch((error) => {
             const message = error.data()
